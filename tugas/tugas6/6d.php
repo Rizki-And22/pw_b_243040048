52567d999bc7361ccd4d8c383bd70f2773884f08
Rizki-And22 <anangkc12@gmail.com>
2025-04-01 20:36:22 +0700

    tugas6

---
+++ tugas/tugas6/6d.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rizki Agustian</title>
+</head>
+
+<body>
+    <h2>Selamat Datang Rizki!</h2>
+</body>
+
+</html>
